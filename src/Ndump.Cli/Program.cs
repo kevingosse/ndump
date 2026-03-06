015bbf5b92dfb4cac101827106a1ed4816d1a8cb
Kevin Gosse <krix33@gmail.com>
2026-03-06 20:21:21 +0100

    Create project structure

---
+++ src/Ndump.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ndump.Cli;
+
+internal class Program
+{
+    static void Main()
+    {
+    }
+}
---
+++ src/Ndump.Gui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ndump.Gui;
+
+internal class Program
+{
+    static void Main()
+    {
+    }
+}
